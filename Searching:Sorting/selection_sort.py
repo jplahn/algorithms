7b6703af9cef2ee4e43b2af03ab0b5d37b522560
Jordan Plahn <jplahn@vt.edu>
2015-04-18 18:46:07 -0400

    Selection sort'

---
+++ Searching:Sorting/selection_sort.py
@@ -0,0 +1,19 @@
+def selection_sort(array, n):
+    # increment boundary of unsorted array
+    for i in range(0, n):
+        # find minimum element in unsorted array
+        min_elem = i
+        for j in range(i+1, n):
+            if (array[j] < array[min_elem]):
+                min_elem = j
+        # swap two elements
+        array[min_elem], array[i] = array[i], array[min_elem]
+
+def main():
+    array = [3, 2, 6, 8, 1, 4, 5]
+    n = len(array)
+    print 'Unsorted array: {}'.format(array)
+    selection_sort(array, n)
+    print 'Sorted array: {}'.format(array)
+
+main()
