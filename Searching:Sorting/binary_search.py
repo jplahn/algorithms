9f3dae8bf0a3225e8e56bb8f6f75331be0d9d594
Jordan Plahn <jplahn@vt.edu>
2015-04-18 17:06:27 -0400

    Binary search

---
+++ Searching:Sorting/binary_search.py
@@ -0,0 +1,25 @@
+# Performs a binary search on a sorted array.
+def bin_search(array, left, right, value):
+    if (right >= left):
+        mid = left + (right - left) / 2;
+
+        # found value
+        if (array[mid] == value):
+            return mid
+
+        # search left half if mid value is greater than value
+        if (array[mid] > value):
+            return bin_search(array, left, mid - 1, value)
+
+        # search right half of array
+        return bin_search(array, mid + 1, right, value)
+
+    return -1
+
+def main():
+    array = [1, 2, 4, 8, 12, 13, 17]
+    n = len(array)
+    value = 13
+    print 'Location of {} is {}'.format(value, bin_search(array, 0, n - 1, value))
+
+main()
